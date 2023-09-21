06c806a7029b15281bd202662d4f64e8992daca3
Jimenez3214 <jimenezeduardo83@gmail.com>
2023-09-20 19:58:01 -0500

    Cleaned up the index and fixed the game loop and added more questions

--- index.js
+++ index.js
@@ -1,153 +1,78 @@
-let question = {
-  title: 'gato',
-  alternatives: ['dog', 'cat', 'bird', 'fish'],
-  correctAnswer: 1
-};
-
-let questions = [
-  {
-    title: 'gato',
-    alternatives: ['dog', 'cat', 'bird', 'fish'],
-    correctAnswer: 1
-  },
-  {
-    title: 'ave',
-    alternatives: ['mouse', 'hamster', 'lizard', 'bird'],
-    correctAnswer: 3
-  },
-  {
-    title: 'rata',
-    alternatives: ['cat', 'fish', 'rat', 'shark'],
-    correctAnswer: 2
-  },
-  {
-    title: 'mosca',
-    alternatives: ['fly', 'puma', 'fish', 'dog'],
-    correctAnswer: 0
-  },
-  {
-      title: 'pescado',
-      alternatives: ['dog', 'hamster', 'fish', 'rat'],
-      correctAnswer: 2
-    },
-    {
-      title: 'Oveja',
-      alternatives: ['Parrot', 'Dove', 'Goose', 'Sheep'],
-      correctAnswer: 3
-    }
-];
+import questions from './questions.js';
 
 let app = {
-
   currentPosition: 0,
-  
+  score: 0,
+
   start: function() {
-    this.currPosition = 0;
-    this.score = 0; 
-  
-    let alts = document.querySelectorAll('.alternative');
-  
-    alts.forEach((element, index) => {
-            
-      element.addEventListener('click', () => {
-  
-        this.checkAnswer(index);
-      });
-    });
-
+    this.score = 0;
+    this.currentPosition = 0;
     this.updateStats();
-    
-    this.showQuestion(questions[this.currPosition]);
+    this.showQuestion();
   },
-  
-  showQuestion: function(q) {
 
-    let titleDiv = document.getElementById('title');
-    titleDiv.textContent = q.title; 
-
+  showQuestion: function() {
+    let question = questions[this.currentPosition];
+    document.getElementById('title').textContent = question.title;
     let alts = document.querySelectorAll('.alternative');
-  
-    alts.forEach(function(element, index){
-      element.textContent = q.alternatives[index];
+    alts.forEach(function(element, index) {
+      element.textContent = question.alternatives[index];
     });
-
-    let questionNumberDiv = document.getElementById('questionNumber');
-    questionNumberDiv.textContent = `Question ${this.currPosition + 1} of ${questions.length}`;
+    document.getElementById('questionNumber').textContent = `Question ${this.currentPosition + 1} of ${questions.length}`;
   },
-  
+
+  handleAnswerClick: function(index) {
+    this.checkAnswer(index);
+  },
+
   checkAnswer: function(userSelected) {
+    let currQuestion = questions[this.currentPosition];
+    let correctAnswer = currQuestion.correctAnswer;
     
-    let currQuestion = questions[this.currPosition];
-    
-    if(currQuestion.correctAnswer == userSelected) {
-
-      console.log('correct');
+    if (correctAnswer == userSelected) {
+      console.log('Correct');
       this.score++;
-      this.showResult(true);
-    }
-    else {
- 
-      console.log('wrong');
-      this.showResult(false);
-    }
-
-    this.updateStats();
-
-    this.increasePosition();
-
-    if (this.currPosition == questions.length) {
-      this.endGame();
     } else {
-
-      this.showQuestion(questions[this.currPosition]);
+      console.log('Wrong');
+      let resultDiv = document.getElementById('result');
+      resultDiv.textContent = `Wrong! Correct answer: ${currQuestion.alternatives[correctAnswer]}`;
     }
 
-  },
-  
-  increasePosition: function() {
-    this.currPosition++;
-    
-    if(this.currPosition == questions.length){
-      this.currPosition = 0;
+    if (this.currentPosition < questions.length - 1) {
+      this.currentPosition++;
+      this.showQuestion();
+    } else {
+      this.endGame();
     }
+    this.updateStats();
   },
-  
+
   updateStats: function() {
     let scoreDiv = document.getElementById('score');
     scoreDiv.textContent = `Your score: ${this.score}`;
   },
-  
-  showResult: function(isCorrect) {
- 
-    let resultDiv = document.getElementById('result');
-    let result = '';
 
-    if(isCorrect) {
-      result = 'Correct Answer!';
-    }
-    else {
-     
-      let currQuestion = questions[this.currPosition];
-      
-      let correctAnswerIndex = currQuestion.correctAnswer;
-      
-      let correctAnswerText = currQuestion.alternatives[correctAnswerIndex];
-      
-      result = `Wrong! Correct answer: ${correctAnswerText}`;
-    }
-    
-    resultDiv.textContent = result;
-    
-  },
+  endGame: function() {
+    let modalContent = document.getElementById('modal-content');
+    modalContent.innerHTML = `Game Over! Your final score: ${this.score} out of ${questions.length}`;
+    let overlay = document.getElementById('overlay');
+    overlay.style.display = 'block';
 
-  endGame: function () {
-      let resultDiv = document.getElementById('result');
-      resultDiv.textContent = `Game Over! Your final score: ${this.score} out of ${questions.length}`;
-    
-      this.currPosition = 0;
-      this.score = 0;
-    },
-  
+    let modalRestart = document.getElementById('modal-restart');
+    modalRestart.textContent = 'Restart';
+    modalRestart.addEventListener('click', () => {
+      overlay.style.display = 'none';
+      this.start();
+    });
+  }
 };
+
+// Event listener for answer clicks
+let alts = document.querySelectorAll('.alternative');
+alts.forEach((element, index) => {
+  element.addEventListener('click', () => {
+    app.handleAnswerClick(index);
+  });
+});
 
 app.start();
---
+++ questions.js
@@ -0,0 +1,99 @@
+let questions = [
+    {
+      title: 'gato',
+      alternatives: ['dog', 'cat', 'bird', 'fish'],
+      correctAnswer: 1
+    },
+    {
+      title: 'ave',
+      alternatives: ['mouse', 'hamster', 'lizard', 'bird'],
+      correctAnswer: 3
+    },
+    {
+      title: 'rata',
+      alternatives: ['cat', 'fish', 'rat', 'shark'],
+      correctAnswer: 2
+    },
+    {
+      title: 'mosca',
+      alternatives: ['fly', 'puma', 'fish', 'dog'],
+      correctAnswer: 0
+    },
+    {
+        title: 'pescado',
+        alternatives: ['dog', 'hamster', 'fish', 'rat'],
+        correctAnswer: 2
+      },
+      {
+        title: 'Oveja',
+        alternatives: ['Parrot', 'Dove', 'Goose', 'Sheep'],
+        correctAnswer: 3
+      },
+      {
+        title: 'perro',
+        alternatives: ['dog', 'cat', 'bird', 'fish'],
+        correctAnswer: 0
+      },
+      {
+        title: 'elefante',
+        alternatives: ['elephant', 'tiger', 'lion', 'giraffe'],
+        correctAnswer: 0
+      },
+      {
+        title: 'caballo',
+        alternatives: ['horse', 'cow', 'pig', 'sheep'],
+        correctAnswer: 0
+      },
+      {
+        title: 'tigre',
+        alternatives: ['tiger', 'lion', 'cheetah', 'jaguar'],
+        correctAnswer: 0
+      },
+      {
+        title: 'jirafa',
+        alternatives: ['giraffe', 'elephant', 'zebra', 'rhinoceros'],
+        correctAnswer: 0
+      },
+      {
+        title: 'mono',
+        alternatives: ['monkey', 'gorilla', 'chimp', 'lemur'],
+        correctAnswer: 0
+      },
+      {
+        title: 'serpiente',
+        alternatives: ['snake', 'lizard', 'crocodile', 'turtle'],
+        correctAnswer: 0
+      },
+      {
+        title: 'tortuga',
+        alternatives: ['turtle', 'snail', 'crab', 'shark'],
+        correctAnswer: 0
+      },
+      {
+        title: 'pato',
+        alternatives: ['duck', 'swan', 'goose', 'heron'],
+        correctAnswer: 0
+      },
+      {
+        title: 'abeja',
+        alternatives: ['bee', 'butterfly', 'ant', 'spider'],
+        correctAnswer: 0
+      },
+      {
+        title: 'araña',
+        alternatives: ['spider', 'scorpion', 'centipede', 'bee'],
+        correctAnswer: 0
+      },
+      {
+        title: 'ciervo',
+        alternatives: ['deer', 'moose', 'elk', 'antelope'],
+        correctAnswer: 0
+      },
+      {
+        title: 'delfín',
+        alternatives: ['dolphin', 'whale', 'shark', 'seal'],
+        correctAnswer: 0
+      }
+  ];
+
+  export default questions;
